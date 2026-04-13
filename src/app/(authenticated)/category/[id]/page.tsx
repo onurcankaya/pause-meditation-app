f615426ace8565057249c066e2d0673ac37953d6
Onur <onur.eren.cankaya@gmail.com>
2026-04-13 11:00:41 +0200

    feat: add completion status to level cards

--- src/app/(authenticated)/category/[id]/page.tsx
+++ src/app/(authenticated)/category/[id]/page.tsx
@@ -7,6 +7,7 @@ import QueryState from '@/components/common/QueryState';
 import LevelCard from '@/components/LevelCard';
 import { useCategory } from '@/hooks/useCategories';
 import { useMeditations } from '@/hooks/useMeditations';
+import { useMeditationProgress } from '@/hooks/useMeditations';
 import { Meditation } from '@/api/types/meditation';
 
 export default function CategoryPage() {
@@ -25,40 +26,70 @@ export default function CategoryPage() {
     error: errorMeditations,
   } = useMeditations(categoryId);
 
+  const {
+    data: meditationProgress,
+    isLoading: isLoadingMeditationProgress,
+    error: errorMeditationProgress,
+  } = useMeditationProgress();
+
+  const isLoading = useMemo(() => {
+    return (
+      isLoadingCategory || isLoadingMeditations || isLoadingMeditationProgress
+    );
+  }, [isLoadingCategory, isLoadingMeditations, isLoadingMeditationProgress]);
+
+  const hasError = useMemo(() => {
+    return errorCategory || errorMeditations || errorMeditationProgress;
+  }, [errorCategory, errorMeditations, errorMeditationProgress]);
+
   const meditationsByLevel = useMemo(() => {
     if (!meditations) return {};
 
-    const map: Record<number, Meditation[]> = {};
+    const map: Record<number, Meditation['id'][]> = {};
 
     for (const meditation of meditations) {
       if (!map[meditation.level]) {
-        map[meditation.level] = [meditation];
-      } else {
-        map[meditation.level].push(meditation);
+        map[meditation.level] = [];
       }
+      map[meditation.level].push(meditation.id);
     }
 
     return map;
   }, [meditations]);
 
+  const completedMeditationIds = useMemo(() => {
+    return meditationProgress?.map(
+      (completedMeditation) => completedMeditation.meditation_id,
+    );
+  }, [meditationProgress]);
+
+  const levels = useMemo(() => {
+    return Object.entries(meditationsByLevel).map(([level, meditations]) => ({
+      level: level,
+      isCompleted: meditations.every((meditation) =>
+        completedMeditationIds?.includes(meditation),
+      ),
+    }));
+  }, [meditationsByLevel, meditationProgress]);
+
   return (
     <PageWrapper
       title={category?.name}
       description={category?.description}
+      isLoading={isLoading}
       showBackButton
     >
-      <QueryState
-        isLoading={isLoadingCategory || isLoadingMeditations}
-        error={errorCategory || errorMeditations}
-      >
+      <QueryState isLoading={isLoading} error={hasError}>
         <div className="w-full space-y-4">
           {category &&
-            Object.entries(meditationsByLevel).map((item, index) => {
-              const [level, meditations] = item;
-              return (
-                <LevelCard key={index} category={category} level={level} />
-              );
-            })}
+            levels.map((level) => (
+              <LevelCard
+                key={`${category}-${level.level}`}
+                category={category}
+                level={level.level}
+                isCompleted={level.isCompleted}
+              />
+            ))}
         </div>
       </QueryState>
     </PageWrapper>
